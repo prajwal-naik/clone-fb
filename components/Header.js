f4d40267957dc0c207b4cfda151ab32016aee273
prajwal-naik <naikprajwal40@gmail.com>
2021-06-11 14:01:43 +0530

    header-left header-complete

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ function Header(){
                 <div className = "flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className = "h-6 text-gray-600"/>
                     <input 
-                        className = "flex ml-2 items-center bg-transparent outline-none flex-shrink placeholder-gray-500 flex-shrink" 
+                        className = "hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none flex-shrink placeholder-gray-500 flex-shrink" 
                         type = "text" 
                         placeholder = "Search Facebook"
                     />
@@ -48,10 +48,14 @@ function Header(){
             </div>
 
             {/* Right */}
-            <div>
-                <Image
+            <div className = "flex items-center sm:spacing-x-2 justify-end">
+                {/* Profile Pic */}
                     
-                />
+                <p className = "whitespace-nowrap font-semibold pr-3"> Prajwal Naik </p>
+                <ViewGridIcon className = "icon" />
+                <ChatIcon className = "icon" />
+                <BellIcon className = "icon" />
+                <ChevronDownIcon className = "icon" />
             </div>
 
 
