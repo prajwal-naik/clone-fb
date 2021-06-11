3e58aa0dd9c993a99dea7050fd30d8adbcc87c26
prajwal-naik <naikprajwal40@gmail.com>
2021-06-11 13:50:32 +0530

    header-center

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,15 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import  HeaderIcon from "./HeaderIcon";
 
 function Header(){
     return (
-        <div>
+        <div className = "sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+            {/* Left */}
             <div className = "flex items-center">
-                <Image src="https://links.papareact.com/5me"
+                <Image 
+                    src="https://links.papareact.com/5me"
                     width = {40}
                     height = {40}
                     layout = "fixed"
@@ -26,13 +29,32 @@ function Header(){
                 <div className = "flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className = "h-6 text-gray-600"/>
                     <input 
-                        className = "flex ml-2 items-center bg-transparent outline-none" 
+                        className = "flex ml-2 items-center bg-transparent outline-none flex-shrink placeholder-gray-500 flex-shrink" 
                         type = "text" 
-                        placeholder = "Search Facebook"/>
+                        placeholder = "Search Facebook"
+                    />
                 </div>
             </div>
 
-  
+            {/* Center */}
+            <div className = "flex justify-center flex-grow">
+                <div className = "flex space-x-6 md:space-x-2">
+                    <HeaderIcon active Icon = {HomeIcon}/>
+                    <HeaderIcon Icon = {FlagIcon}/>
+                    <HeaderIcon Icon = {PlayIcon}/>
+                    <HeaderIcon Icon = {ShoppingCartIcon}/>
+                    <HeaderIcon Icon = {UserGroupIcon}/>
+                </div>
+            </div>
+
+            {/* Right */}
+            <div>
+                <Image
+                    
+                />
+            </div>
+
+
         </div>
     );
 }
